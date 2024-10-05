d424a40cdd8423937ac088be2329e0ad13d7eba4
rushitvarmag <95670125+rushitvarmag@users.noreply.github.com>
2024-10-04 22:50:36 -0400

    Update main.py

--- project0/main.py
+++ project0/main.py
@@ -45,33 +45,20 @@ def extract_incidents(file_path):
    # PDF Loading
    reader = PdfReader(file_path)
 
-   # extracting text data from all pages
    text_data = [page.extract_text(extraction_mode="layout") for page in reader.pages]
 
-   # Parsing all the incidents using double spaces (more than one double space can be used) to separate columns
    incidents = []
-   # Variable to track if the header is skipped or not
-   header_skipped = False
+   ignore_header = False
 
    for page_text in text_data:
-       # Splitting the page text into lines
        lines = page_text.split('\n')
        for line in lines:
-           # Using double spaces for splitting of columns
-           columns = re.split(r'\s{2,}', line.strip())
-
-           # Ensuring that all columns exist to match the expected number of fields
-           if len(columns) >= 5:
-               # Skip the first row if it is the header
-               if not header_skipped:
-                   header_skipped = True
+           c = re.split(r'\s{2,}', line.strip())
+           if len(c) >= 5:
+               if not ignore_header:
+                   ignore_header = True
                    continue
-
-               # Opening the columns in the following order:
-               # Date / Time | Incident Number | Location | Nature | Incident ORI
-               date_time, incident_number, location, nature, incident_ori = map(str.strip, columns[:5])
-
-               # Appending incident data as a dictionary
+               date_time, incident_number, location, nature, incident_ori = map(str.strip, c[:5])
                incidents.append({
                    'incident_time': date_time,
                    'incident_number': incident_number,
